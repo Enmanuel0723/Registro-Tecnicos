161a3b610e1b8b39367f47a4a007117f6a346ed1
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-01-21 14:57:19 -0400

    Se creo el contexto

---
+++ DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Registro_Tecnicos.Models;
+using Microsoft.EntityFrameworkCore;
+using Registro;
+using System.Collections.Generic;
+
+namespace Registro_Tecnicos.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+
+    }
+}
